f9922ec12a0de1004e40a27b0061db2ef972dd7e
dB <smithwebdd@gmail.com>
2021-03-20 18:57:47 -0400

    Completion, Fold, Line, Window/Buffer/Tab Options created

--- nvim/lua/smithwebdev/std_settings.lua
+++ nvim/lua/smithwebdev/std_settings.lua
@@ -1,8 +1,24 @@
+-- =============================================================================
+-- Standard Settings Helper --
+-- =============================================================================
+
 local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 local function set(scope, key, value)
-	scopes[scope][key] = value
+	scopes[scope][key] = valu
 	if scope ~= 'o' then scopes['o'][key] = value end
 end
+
+--==============================================================================
+
+-- Completion Options
+set('o', 'completeopt', 'menuone,noinsert,noselect')
+set('o', 'updatetime', 3000)
+
+-- Fold Options
+set('w', 'foldmethod', 'indent')
+set('w', 'foldnestmax', 3)
+set('w', 'foldenable', false)
+set('w', 'foldlevel', 99)
 
 -- Indentation Options
 local indent = 2
@@ -11,14 +27,25 @@ set('b', 'expandtab', true)
 set('b', 'tabstop', indent)
 set('b', 'shiftwidth', indent)
 set('b', 'smartindent', true)
-set('b', 'smarttab', true)
 set('b', 'softtabstop', indent)
+set('o', 'smarttab', true)
+
+-- Line Options
+set('b', 'textwidth', 120)
+set('o', 'showbreak', '+++')
+set('o', 'scrolloff', 999)
+set('o', 'sidescroll', 1)
+set('o', 'sidescrolloff', 15)
+set('w', 'list', true)
+set('w', 'listchars', 'tab:→ ,trail:·,extends:…,eol:↩')
+
 
 -- Search Options
 set('o', 'hlsearch', true)
 set('o', 'ignorecase', true)
 set('o', 'incsearch', true)
 set('o', 'smartcase', true)
+set('o', 'path', '+=**')
 set('o', 'wrapscan', true)
 
 -- File 
@@ -30,4 +57,15 @@ set('o', 'termguicolors', true)
 -- Line Numbering
 set('w', 'number', true)
 set('w', 'relativenumber', true)
+
+-- Window/Buffer/Tab options
+set('o', 'hidden', true)
+set('o', 'cmdheight', 1)
+set('o', 'pumheight', 10)
+set('o', 'splitbelow', true)
+set('o', 'splitright', true)
+set('o', 'backup', false)
+set('o', 'writebackup', false)
+
+
 
