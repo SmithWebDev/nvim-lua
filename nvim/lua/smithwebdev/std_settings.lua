6c509d4554c49959001877261d481c35867e527f
dB <smithwebdd@gmail.com>
2021-03-20 13:16:48 -0400

    added search options and indentation options

--- nvim/lua/smithwebdev/std_settings.lua
+++ nvim/lua/smithwebdev/std_settings.lua
@@ -1,10 +1,33 @@
-local o=vim.o   -- Options
-local bo=vim.bo -- Buffer Options
-local wo=vim.wo -- Window Options
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+local function set(scope, key, value)
+	scopes[scope][key] = value
+	if scope ~= 'o' then scopes['o'][key] = value end
+end
 
-o.syntax = 'on'
-o.termguicolors = true
+-- Indentation Options
+local indent = 2
+set('b', 'autoindent', true)
+set('b', 'expandtab', true)
+set('b', 'tabstop', indent)
+set('b', 'shiftwidth', indent)
+set('b', 'smartindent', true)
+set('b', 'smarttab', true)
+set('b', 'softtabstop', indent)
 
+-- Search Options
+set('o', 'hlsearch', true)
+set('o', 'ignorecase', true)
+set('o', 'incsearch', true)
+set('o', 'smartcase', true)
+set('o', 'wrapscan', true)
 
+-- File 
+set('o', 'autowrite', true)
+set('o', 'autowriteall', true)
+set('o', 'mouse', 'a')
+set('o', 'termguicolors', true)
 
-wo.relativenumber = true
+-- Line Numbering
+set('w', 'number', true)
+set('w', 'relativenumber', true)
+
